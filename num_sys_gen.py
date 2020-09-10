8d156616970dfd0c1438f9de1b5bbba1b6787ca4
Dima <70443041+candiel-z@users.noreply.github.com>
2020-09-10 21:28:42 +0300

    Update num_sys_gen.py
    Refactored using OOP paradigm
    Added: 
    custom_alphabet_length,
    disable_negative_numbers

--- num_sys_gen.py
+++ num_sys_gen.py
@@ -1,94 +1,114 @@
-alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',     # anything except '-'
+ALPHABET = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',     # anything except '-'
             'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',     # anything except '-'
             'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',     # anything except '-'
             'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']               # anything except '-'
-output_length = 2 # desired code string length
-
-alphabet_length = len(alphabet)
-unsigned_range = alphabet_length**output_length - 1
 
 
-def encrypt(num: int, remove_unnecessary_chars=False, disable_out_of_range_exception=False) -> str:
-    """Encrypts input integer to the string"""
+class NumeralSystemGenerator(object):
+    def __init__(self, 
+    max_output_length=6,
+    custom_alphabet_length=len(ALPHABET),
+    disable_negative_numbers=False,
+    disable_out_of_range_exception=False,
+    remove_unnecessary_chars=False,
+    ):
+        """
+        max_output_length --                maximum output code string length not including '-'\n
+        custom_alphabet_length --           slice length from ALPHABET\n
+        disable_negative_numbers --         num < 0 will raise 'Input number out of range' exception\n
+        disable_out_of_range_exception --   num > range will be counted from the begginning (130/(-127 -- 128) -> 1)\n
+        remove_unnecessary_chars --         remove unused characters from the code string (-000F0 -> -F0)
+        """
 
-    if (num < -unsigned_range or num > unsigned_range) and not disable_out_of_range_exception:
-        raise Exception('Input number out of range')
-    
-    res = ''
+        self.alphabet = ALPHABET[:custom_alphabet_length]
+        self.max_output_length = max_output_length
 
-    if num < 0:
-        res = '-' + _encrypt_handler(-num) # -(-num) = +num
-    else:
-        res = _encrypt_handler(num)
+        self.disable_negative_numbers = disable_negative_numbers
+        self.disable_out_of_range_exception = disable_out_of_range_exception
+        self.remove_unnecessary_chars = remove_unnecessary_chars
 
-    if remove_unnecessary_chars:
-        res = _remove_unnecessary_chars(res)
-        if res == '': return alphabet[0]
-        else: return res
-    else:
+        self.alphabet_length = len(self.alphabet)
+        self.unsigned_range = self.alphabet_length**self.max_output_length - 1
+
+    def encrypt(self, num: int) -> str:
+        """Encrypt input integer to code string"""
+
+        if (((num < -self.unsigned_range or num > self.unsigned_range) or 
+            ((num < 0 or num > self.unsigned_range) and self.disable_negative_numbers)) and 
+              not self.disable_out_of_range_exception):
+            raise Exception('Input number out of range')
+
+        res = ''
+
+        if num < 0:
+            res = '-' + self._encrypt_handler(-num) # -(-num) = +num
+        else:
+            res = self._encrypt_handler(num)
+
+        if self.remove_unnecessary_chars:
+            res = self._remove_unnecessary_chars(res)
+            if res == '': return self.alphabet[0]
+            else: return res
+        else:
+            return res
+
+    def decrypt(self, code: str) -> int:
+        """Decrypt input code string to integer"""
+
+        if len(code.replace('-', '')) > self.max_output_length or len(code.replace('-', '')) == 0:
+            raise Exception('Code length out of range')
+        if not any(symbol in code for symbol in self.alphabet):
+            raise Exception('Unknown symbols. This characters aren`t in the alphabet')
+        
+        if len(code.replace('-', '')) < self.max_output_length:
+            code = self._add_unnecessary_chars(code) # if the string has the wrong length it will break the decryption
+
+        if code[0] == '-':
+            return -self._decrypt_handler(code[1:])
+        else:
+            return self._decrypt_handler(code)
+
+    def get_range(self):
+        """Return range as dict {'min': min, 'max': max}"""
+
+        return {'min': -self.unsigned_range,
+                'max': self.unsigned_range}
+
+    def _encrypt_handler(self, num: int) -> str:
+        """Hadle encrypting input string"""
+
+        res = ''
+        while len(res) != self.max_output_length:
+            res += self.alphabet[num % self.alphabet_length]
+            num = int(num / self.alphabet_length)
+        return res[::-1]
+
+
+    def _decrypt_handler(self, code: str) -> int:
+        """Handle decrypting input string"""
+
+        res = 0
+        for i in range(self.max_output_length):
+            res += self.alphabet.index(code[i]) * self.alphabet_length**((self.max_output_length - 1) - i)
         return res
 
+    def _remove_unnecessary_chars(self, code: str) -> str:
+        """Remove unnecessary characters from code string"""
 
-def decrypt(code: str) -> int:
-    """Decrypts input string to an integer"""
+        res = [char for char in code]
 
-    if len(code.replace('-', '')) > output_length or len(code.replace('-', '')) == 0:
-        raise Exception('Code length out of range')
-    if not any(symbol in code for symbol in alphabet):
-        raise Exception('Unknown symbols. This characters aren`t in the alphabet')
-    
-    if len(code.replace('-', '')) < output_length:
-        code = _add_unnecessary_chars(code) # if the string has the wrong length it will break the decryption
-
-    if code[0] == '-':
-        return -_decrypt_handler(code[1:])
-    else:
-        return _decrypt_handler(code)
+        for i in range(len(res)):
+            if res[i] == self.alphabet[0]: res[i] = ''
+            elif res[i] == '-': continue
+            else: break
+        return ''.join(res)
 
 
-def get_range():
-    """Return dictionary {'min': min, 'max': max}"""
+    def _add_unnecessary_chars(self, code: str) -> str:
+        """Return full length code (include unnecessary chars)"""
 
-    return {'min': -unsigned_range,
-            'max': unsigned_range}
-
-
-def _encrypt_handler(num: int) -> str:
-    """Hadle encrypting input string"""
-
-    res = ''
-    while len(res) != output_length:
-        res += alphabet[num % alphabet_length]
-        num = int(num / alphabet_length)
-    return res[::-1]
-
-
-def _decrypt_handler(code: str) -> int:
-    """Handle decrypting input string"""
-
-    res = 0
-    for i in range(output_length):
-        res += alphabet.index(code[i]) * alphabet_length**((output_length - 1) - i)
-    return res
-
-
-def _remove_unnecessary_chars(code: str) -> str:
-    """Remove unnecessary characters from code string"""
-
-    res = [char for char in code]
-
-    for i in range(len(res)):
-        if res[i] == alphabet[0]: res[i] = ''
-        elif res[i] == '-': continue
-        else: break
-    return ''.join(res)
-
-
-def _add_unnecessary_chars(code: str) -> str:
-    """Return full length code (include unnecessary chars)"""
-
-    res = ''
-    if code[0] == '-': res += '-'
-    res += alphabet[0]*(output_length - len(code.replace('-', '')))
-    res += code.replace('-', '')
-    return res
+        res = ''
+        if code[0] == '-': res += '-'
+        res += self.alphabet[0]*(self.max_output_length - len(code.replace('-', '')))
+        res += code.replace('-', '')
+        return res
