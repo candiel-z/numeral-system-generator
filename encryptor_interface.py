185188449b1f82159d3ec4e0dee1ac19b0048a59
Dima <70443041+candiel-z@users.noreply.github.com>
2020-09-30 15:53:07 +0300

    Update encryptor_interface.py

--- encryptor_interface.py
+++ encryptor_interface.py
@@ -1,26 +1,29 @@
 from encryptor import Encryptor
-from config import ALPHABET, MAX_OUTPUT_LEN
+from config import ALPHABET
 
 
 class EncryptorInterface(Encryptor):
     def __init__(self, 
         alphabet_len=len(ALPHABET),
+        max_output_len=6,
         remove_unnecessary_chars=False,
     ):
         """
-        
+        alphabet_len -- slice length of ALPHABET\n
+        autoincrement_step -- step for generator\n
+        remove_unnecessary_chars -- if True remove unused chars from code string (e.g. -00Fg0 -> -Fg0)
         """
 
         self.alphabet_len = alphabet_len
         self.remove_unnecessary_chars = remove_unnecessary_chars
 
-        self.unsigned_range = self.alphabet_len**MAX_OUTPUT_LEN - 1
+        self.unsigned_range = self.alphabet_len**max_output_len - 1
         self.io_int_range = {'min': -self.unsigned_range, 'max': self.unsigned_range}
 
         super(EncryptorInterface, self).__init__(alphabet_len)
 
     def encrypt(self, num: int) -> str:
-        """"""
+        """Handle encrypting with negative numbers and removing unnecessary chars"""
     
         if num < self.io_int_range['min'] or num > self.io_int_range['max']:
             raise Exception('Input integer out of range')
@@ -37,7 +40,7 @@ class EncryptorInterface(Encryptor):
         return code
 
     def decrypt(self, code: str) -> int:
-        """"""
+        """Return decrypted input code string to integer"""
 
         if code[0] == '-': 
             return -self._Encryptor__decrypt(code[1:])
@@ -45,7 +48,7 @@ class EncryptorInterface(Encryptor):
             return self._Encryptor__decrypt(code)
 
     def _remove_unnecessary_chars(self, code: str) -> str:
-        """"""
+        """Return code string without unnecessary chars"""
 
         res = [char for char in code]
 
@@ -57,12 +60,16 @@ class EncryptorInterface(Encryptor):
         return ''.join(res)
 
     def __get_range(self) -> dict:
-        """"""
+        """Return a range as dict {'min': integer, 'max': integer}"""
 
         return self.io_int_range
 
-    def __set_range(self, min: int, max: int):
-        """"""
+    def __set_range(self, min: int, max: int) -> None:
+        """
+        min -- minimal value of range\n
+        max -- maximal value of range\n
+        max cannot be less than min
+        """
 
         if min > max:
             raise Exception("'max' arg cannot be less than 'min'")
