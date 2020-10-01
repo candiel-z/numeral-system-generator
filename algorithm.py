e1818815bbbc9f27b5a454ad8c6a0b7d346d9a59
Dima <70443041+candiel-z@users.noreply.github.com>
2020-10-01 15:17:25 +0300

    Add files via upload
    Just an algorithm

---
+++ algorithm.py
@@ -0,0 +1,33 @@
+ALPHABET = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',     
+            'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',     
+            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',     
+            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']               
+
+
+alphabet_len = 62 # len(ALPHABET)
+alphabet = ALPHABET[:alphabet_len]
+
+def encrypt(num: int) -> str:
+    """
+    Return input decimal integer as code string
+    """
+
+    res = ''
+    while True:
+        if num == 0:      
+            break
+        res += alphabet[num % alphabet_len]
+        num = int(num / alphabet_len)
+    return res[::-1]
+
+def decrypt(code: str) -> int:
+    """
+    Return input code string as decimal integer.
+    """
+
+    res = 0
+    code = code[::-1]
+
+    for i in range(len(code)):   
+        res += alphabet.index(code[i]) * alphabet_len**i
+    return res
