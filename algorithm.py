85ef4d412c0209713bd052ea8d0514670af4f7b5
Dima <70443041+candiel-z@users.noreply.github.com>
2020-10-05 08:26:46 +0300

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -13,9 +13,7 @@ def encrypt(num: int) -> str:
     """
 
     res = ''
-    while True:
-        if num == 0:      
-            break
+    while num == 0:
         res += alphabet[num % alphabet_len]
         num = int(num / alphabet_len)
     return res[::-1]
